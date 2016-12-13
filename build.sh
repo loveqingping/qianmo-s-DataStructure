8b0fbf0f87bf00ffe4cf049dc9d2f4663f5f2237
loveqingping <ilovervin@hotmail.com>
2016-12-13 13:42:05 +0800

    Delete build.sh

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-#  Filename: build.sh
-#   Created: 2016-12-07 14:29:41
-#      Desc: TODO (some description)
-#    Author: dusk(qianmo du), dusk@ifeng.com
-#   Company: Ifeng
-gcc -o test_graph -g3 list_graph.c
-gcc -o test_mst -g3 matrix_graph.c
